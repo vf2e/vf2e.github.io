af1a921ab5d10e8d0cbc4095714e54f22935aeeb
wangchang <wangchang@znion.com>
2019-07-23 09:31:19 +0800

    Site updated: 2019-07-23 09:31:17

--- js/search.js
+++ js/search.js
@@ -1,3 +1,23 @@
+// A local search script with the help of [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
+// Copyright (C) 2015 
+// Joseph Pan <http://github.com/wzpan>
+// Shuhao Mao <http://github.com/maoshuhao>
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+// 
+
 var searchFunc = function(path, search_id, content_id) {
     'use strict';
     $.ajax({
@@ -12,8 +32,11 @@ var searchFunc = function(path, search_id, content_id) {
                     url: $( "url" , this).text()
                 };
             }).get();
+
             var $input = document.getElementById(search_id);
+      if (!$input) return;
             var $resultContent = document.getElementById(content_id);
+
             $input.addEventListener('input', function(){
                 var str='<ul class=\"search-result-list\">';
                 var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
@@ -25,17 +48,21 @@ var searchFunc = function(path, search_id, content_id) {
                 datas.forEach(function(data) {
                     var isMatch = true;
                     var content_index = [];
-                    var data_title = data.title.trim().toLowerCase();
+                    if (!data.title || data.title.trim() === '') {
+                        data.title = "Untitled";
+                    }
+                    var data_title = data.title.trim().toLowerCase();     
                     var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
                     var data_url = data.url;
                     var index_title = -1;
                     var index_content = -1;
                     var first_occur = -1;
-                    // only match artiles with not empty titles and contents
-                    if(data_title != '' && data_content != '') {
+                    // only match artiles with not empty contents
+                    if (data_content !== '') {
                         keywords.forEach(function(keyword, i) {
                             index_title = data_title.indexOf(keyword);
                             index_content = data_content.indexOf(keyword);
+
                             if( index_title < 0 && index_content < 0 ){
                                 isMatch = false;
                             } else {
@@ -45,8 +72,11 @@ var searchFunc = function(path, search_id, content_id) {
                                 if (i == 0) {
                                     first_occur = index_content;
                                 }
+                                // content_index.push({index_content:index_content, keyword_len:keyword_len});
                             }
                         });
+                    } else {
+                        isMatch = false;
                     }
                     // show search results
                     if (isMatch) {
@@ -54,19 +84,23 @@ var searchFunc = function(path, search_id, content_id) {
                         var content = data.content.trim().replace(/<[^>]+>/g,"");
                         if (first_occur >= 0) {
                             // cut out 100 characters
-                            var start = first_occur - 30;
-                            var outLength = 78;
+                            var start = first_occur - 20;
+                            var end = first_occur + 80;
+
                             if(start < 0){
                                 start = 0;
                             }
-                            if (start + outLength > content.length){
-                                if(content.length < outLength){
-                                    outLength = content.length - start;
-                                }else{
-                                    start = content.length - outLength;
-                                }
+
+                            if(start == 0){
+                                end = 100;
                             }
-                            var match_content = content.substr(start, outLength);
+
+                            if(end > content.length){
+                                end = content.length;
+                            }
+
+                            var match_content = content.substr(start, end); 
+
                             // highlight all keywords
                             keywords.forEach(function(keyword){
                                 var regS = new RegExp(keyword, "gi");
